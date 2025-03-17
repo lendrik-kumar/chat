9fa60bbe00e5e65793eb482ba3f63ddf84385356
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-03-17 22:21:30 +0530

    add avatar component and color utility; enhance profile page with image upload and user info display

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,15 +1,94 @@
 import React from 'react'
 import { useAppStore } from '../../store/index.js'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { IoArrowBack } from 'react-icons/io5'
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
+import { getColor } from '../../lib/utils.js'
+import {FaTrash, FaPlus} from 'react-icons/fa'
 
 const Profile = () => {
-
-  const {userInfo} = useAppStore()
-  const [firstName, setName] = useState("")
+  const navigate = useNavigate
+  const {userInfo, setUserInfo} = useAppStore()
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [image, setImage] = useState(null)
+  const [hovered, setHovered] = useState(false)
+  const [selectedColour, setSelectedColour] = useState(0)
 
   return (
-    <div>
-        Profile
-        <div>Email : {userInfo.email} </div>
+    <div className='bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10'>
+      <div className='flex flex-col gap-10 w-[80vw] md:w-max ' >
+        <div>
+          <IoArrowBack className='text-4xl lg:text-6xl text-white/90 cursor-pointer' />
+        </div>
+        <div className='grid grid-cols-2'>
+          <div
+            className='h-full w-32 md:w-48 relative flex items-center justify-center'
+            onMouseEnter={() => setHovered(true)} 
+            onMouseLeave={() => setHovered(false)}
+            >
+            <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              { image ? (
+                <AvatarImage src={image}
+                alt="profile"
+                className="object-cover w-full h-full
+                bg-black"
+                />
+                ) : (
+                <div className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(selectedColour)}`}>
+                { firstName
+                  ? firstName.split("").shift()
+                  : userInfo.email.split("").shift()}
+                </div>
+                )
+              }
+            </Avatar>
+            {
+              hovered && (<div
+              className=' absolute inset-0 flex items-center justify-center bg-black/50 rounded-full'
+              > 
+                {image ? (
+                  <FaTrash className='text-white text-3xl cursor-pointer'/>
+                  ) : ( 
+                  <FaPlus className='text-white text-3xl cursor-pointer'/>)
+                } 
+                </div>
+              )
+            }
+            {/* <input type="text" /> */}
+          </div>
+          <div className='flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center'>
+            <div className='w-full'>
+              <input 
+               placeholder ="Email" 
+               type="email"
+               disabled 
+               value = {userInfo.email}
+               className = "rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+              <div className='w-full'>
+              <input 
+               placeholder ="First Name" 
+               type="text"
+               onChange={(e) => setFirstName(e.target.value)}
+               value = {firstName}
+               className = "rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+              </div>
+              <div className='w-full'>
+              <input 
+               placeholder ="Last Name" 
+               type="text"
+               onChange={(e) => setLastName(e.target.value)}
+               value = {lastName}
+               className = "rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />    
+              </div>         
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
